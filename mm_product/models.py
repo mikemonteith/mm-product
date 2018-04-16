3a6bbe2a1afaa287d564be6db28112885cdded1c
Mike Monteith <mike@mikemonteith.com>
2018-04-16 11:55:38 +0100

    Inherit from the core page model

--- mm_product/models.py
+++ mm_product/models.py
@@ -1,12 +1,10 @@
-from wagtail.wagtailcore.models import Page
-
 from django.db import models
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from mm_core.core.models import CorePage
 
-
-class MikesPage(Page):
+class MikesPage(CorePage):
     old_url = models.CharField(max_length=1024, blank=True)
 
-    content_panels = Page.content_panels + [
+    content_panels = CorePage.content_panels + [
         FieldPanel('old_url'),
     ]
